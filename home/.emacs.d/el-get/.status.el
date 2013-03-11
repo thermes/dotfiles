51f255bf274e352619c052d2e469150619b13134
HORINOUCHI Masato <thermes@confei.to>
2013-03-11 18:17:07 +0900

    delete magit via el-get.

--- home/.emacs.d/el-get/.status.el
+++ home/.emacs.d/el-get/.status.el
@@ -1,22 +1,2 @@
-((color-theme status "installed" recipe
-	      (:name color-theme :description "An Emacs-Lisp package with more than 50 color themes for your use. For questions about color-theme" :website "http://www.nongnu.org/color-theme/" :type http-tar :options
-		     ("xzf")
-		     :url "http://download.savannah.gnu.org/releases/color-theme/color-theme-6.6.0.tar.gz" :load "color-theme.el" :features "color-theme" :post-init
-		     (progn
-		       (color-theme-initialize)
-		       (setq color-theme-is-global t))))
- (color-theme-solarized status "installed" recipe
-			(:name color-theme-solarized :description "Emacs highlighting using Ethan Schoonover's Solarized color scheme" :type github :pkgname "sellout/emacs-color-theme-solarized" :depends color-theme :prepare
-			       (progn
-				 (add-to-list 'custom-theme-load-path default-directory)
-				 (autoload 'color-theme-solarized-light "color-theme-solarized" "color-theme: solarized-light" t)
-				 (autoload 'color-theme-solarized-dark "color-theme-solarized" "color-theme: solarized-dark" t))))
- (el-get status "installed" recipe
-	 (:name el-get :website "https://github.com/dimitri/el-get#readme" :description "Manage the external elisp bits and pieces you depend upon." :type github :branch "4.stable" :pkgname "dimitri/el-get" :features el-get :info "." :load "el-get.el"))
- (magit status "installed" recipe
-	(:name magit :website "https://github.com/magit/magit#readme" :description "It's Magit! An Emacs mode for Git." :type github :pkgname "magit/magit" :info "." :autoloads
-	       ("50magit")
-	       :build
-	       (("make" "all"))
-	       :build/darwin
-	       `(,(concat "make EMACS=" el-get-emacs " all")))))
+((el-get status "installed" recipe
+	 (:name el-get :website "https://github.com/dimitri/el-get#readme" :description "Manage the external elisp bits and pieces you depend upon." :type github :branch "4.stable" :pkgname "dimitri/el-get" :features el-get :info "." :load "el-get.el")))
