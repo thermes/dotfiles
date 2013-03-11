aa2264cf36b74d64a12933fdd1529e40f787ce61
HORINOUCHI Masato <thermes@confei.to>
2013-03-11 16:58:49 +0900

    change font size.

--- home/.emacs.d/inits/cocoa-emacs-font.el
+++ home/.emacs.d/inits/cocoa-emacs-font.el
@@ -1,8 +1,10 @@
+; -*- mode: emacs-lisp; coding: utf-8 -*-
+
 (create-fontset-from-ascii-font
- "Ricty-12:weight=normal:slant=normal" nil "ricty")
+ "Ricty-14:weight=normal:slant=normal" nil "ricty")
 (set-fontset-font "fontset-ricty"
 		  'unicode
-		  (font-spec :family "Ricty" :size 12))
+		  (font-spec :family "Ricty" :size 14))
 (add-to-list 'default-frame-alist '(font . "fontset-ricty"))
 
 
