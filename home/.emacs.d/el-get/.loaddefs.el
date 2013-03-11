5391233daffc8ab99f70e6a0b32f97d29d9de257
HORINOUCHI Masato <thermes@confei.to>
2013-03-11 19:25:28 +0900

    install ddskk via el-get.

--- home/.emacs.d/el-get/.loaddefs.el
+++ home/.emacs.d/el-get/.loaddefs.el
@@ -52,188 +52,46 @@ Display a list of packages.
 
 ;;;***
 
-;;;### (autoloads (magit-status) "magit/magit" "../../../../../../../.emacs.d/el-get/magit/magit.el"
-;;;;;;  "6c27f9c75d97da660c00cb151f566ead")
-;;; Generated autoloads from ../../../../../../../.emacs.d/el-get/magit/magit.el
-
-(autoload 'magit-status "magit/magit" "\
-Open a Magit status buffer for the Git repository containing DIR.
-If DIR is not within a Git repository, offer to create a Git
-repository in DIR.
-
-Interactively, a prefix argument means to ask the user which Git
-repository to use even if `default-directory' is under Git
-control.  Two prefix arguments means to ignore `magit-repo-dirs'
-when asking for user input.
-
-\(fn DIR)" t nil)
-
-;;;***
-
-;;;### (autoloads (magit-blame-mode) "magit/magit-blame" "../../../../../../../.emacs.d/el-get/magit/magit-blame.el"
-;;;;;;  "c87000addc09fcd213292db2b8010256")
-;;; Generated autoloads from ../../../../../../../.emacs.d/el-get/magit/magit-blame.el
-
-(autoload 'magit-blame-mode "magit/magit-blame" "\
-Display blame information inline.
-
-\(fn &optional ARG)" t nil)
-
-;;;***
-
-;;;### (autoloads (turn-on-magit-flow magit-flow-mode) "magit/magit-flow"
-;;;;;;  "magit/magit-flow.el" (20797 36146))
-;;; Generated autoloads from magit/magit-flow.el
-
-(autoload 'magit-flow-mode "magit/magit-flow" "\
-FLOW support for Magit
-
-\(fn &optional ARG)" t nil)
-
-(autoload 'turn-on-magit-flow "magit/magit-flow" "\
-Unconditionally turn on `magit-flow-mode'.
-
-\(fn)" nil nil)
-
-;;;***
-
-;;;### (autoloads (turn-on-magit-stgit magit-stgit-mode) "magit/magit-stgit"
-;;;;;;  "../../../../../../../.emacs.d/el-get/magit/magit-stgit.el"
-;;;;;;  "3a799c21b480259ac5942c12153a0e2f")
-;;; Generated autoloads from ../../../../../../../.emacs.d/el-get/magit/magit-stgit.el
-
-(autoload 'magit-stgit-mode "magit/magit-stgit" "\
-StGit support for Magit
-
-\(fn &optional ARG)" t nil)
-
-(autoload 'turn-on-magit-stgit "magit/magit-stgit" "\
-Unconditionally turn on `magit-stgit-mode'.
-
-\(fn)" nil nil)
-
-;;;***
-
-;;;### (autoloads (turn-on-magit-svn magit-svn-mode) "magit/magit-svn"
-;;;;;;  "../../../../../../../.emacs.d/el-get/magit/magit-svn.el"
-;;;;;;  "0a4ea005c64be13e55a43f4138b03ce6")
-;;; Generated autoloads from ../../../../../../../.emacs.d/el-get/magit/magit-svn.el
-
-(autoload 'magit-svn-mode "magit/magit-svn" "\
-SVN support for Magit
-
-\(fn &optional ARG)" t nil)
-
-(autoload 'turn-on-magit-svn "magit/magit-svn" "\
-Unconditionally turn on `magit-svn-mode'.
-
-\(fn)" nil nil)
-
-;;;***
-
-;;;### (autoloads (turn-on-magit-topgit magit-topgit-mode) "magit/magit-topgit"
-;;;;;;  "../../../../../../../.emacs.d/el-get/magit/magit-topgit.el"
-;;;;;;  "4ab21c607308ffc9dd96dba3f5d06e85")
-;;; Generated autoloads from ../../../../../../../.emacs.d/el-get/magit/magit-topgit.el
-
-(autoload 'magit-topgit-mode "magit/magit-topgit" "\
-Topgit support for Magit
-
-\(fn &optional ARG)" t nil)
-
-(autoload 'turn-on-magit-topgit "magit/magit-topgit" "\
-Unconditionally turn on `magit-topgit-mode'.
-
-\(fn)" nil nil)
-
-;;;***
-
-;;;### (autoloads (global-magit-wip-save-mode magit-wip-save-mode
-;;;;;;  magit-wip-mode) "magit/magit-wip" "../../../../../../../.emacs.d/el-get/magit/magit-wip.el"
-;;;;;;  "b6c10a2fc2c80af4791ab79dbba9a7c4")
-;;; Generated autoloads from ../../../../../../../.emacs.d/el-get/magit/magit-wip.el
-
-(defvar magit-wip-mode nil "\
-Non-nil if Magit-Wip mode is enabled.
-See the command `magit-wip-mode' for a description of this minor mode.
-Setting this variable directly does not take effect;
-either customize it (see the info node `Easy Customization')
-or call the function `magit-wip-mode'.")
-
-(custom-autoload 'magit-wip-mode "magit/magit-wip" nil)
-
-(autoload 'magit-wip-mode "magit/magit-wip" "\
-In Magit log buffers; give wip refs a special appearance.
-
-\(fn &optional ARG)" t nil)
-
-(autoload 'magit-wip-save-mode "magit/magit-wip" "\
-Magit support for committing to a work-in-progress ref.
-
-When this minor mode is turned on and a file is saved inside a writable
-git repository then it is also committed to a special work-in-progress
-ref.
-
-\(fn &optional ARG)" t nil)
-
-(defvar global-magit-wip-save-mode nil "\
-Non-nil if Global-Magit-Wip-Save mode is enabled.
-See the command `global-magit-wip-save-mode' for a description of this minor mode.
-Setting this variable directly does not take effect;
-either customize it (see the info node `Easy Customization')
-or call the function `global-magit-wip-save-mode'.")
-
-(custom-autoload 'global-magit-wip-save-mode "magit/magit-wip" nil)
-
-(autoload 'global-magit-wip-save-mode "magit/magit-wip" "\
-Toggle Magit-Wip-Save mode in all buffers.
-With prefix ARG, enable Global-Magit-Wip-Save mode if ARG is positive;
-otherwise, disable it.  If called from Lisp, enable the mode if
-ARG is omitted or nil.
-
-Magit-Wip-Save mode is enabled in all buffers where
-`turn-on-magit-wip-save' would do it.
-See `magit-wip-save-mode' for more information on Magit-Wip-Save mode.
-
-\(fn &optional ARG)" t nil)
-
-;;;***
-
-;;;### (autoloads (rebase-mode) "magit/rebase-mode" "../../../../../../../.emacs.d/el-get/magit/rebase-mode.el"
-;;;;;;  "54e2a1b94ef11fbf4b56dc1f4910cf9b")
-;;; Generated autoloads from ../../../../../../../.emacs.d/el-get/magit/rebase-mode.el
-
-(autoload 'rebase-mode "magit/rebase-mode" "\
-Major mode for editing of a Git rebase file.
-
-Rebase files are generated when you run 'git rebase -i' or run
-`magit-interactive-rebase'.  They describe how Git should perform
-the rebase.  See the documentation for git-rebase (e.g., by
-running 'man git-rebase' at the command line) for details.
-
-\(fn)" t nil)
-
-(add-to-list 'auto-mode-alist '("git-rebase-todo" . rebase-mode))
-
-;;;***
-
-;;;### (autoloads nil nil ("../../../../../../../.emacs.d/el-get/magit/50magit.el"
-;;;;;;  "../../../../../../../.emacs.d/el-get/magit/magit-bisect.el"
-;;;;;;  "../../../../../../../.emacs.d/el-get/magit/magit-blame.el"
-;;;;;;  "../../../../../../../.emacs.d/el-get/magit/magit-flow.el"
-;;;;;;  "../../../../../../../.emacs.d/el-get/magit/magit-key-mode.el"
-;;;;;;  "../../../../../../../.emacs.d/el-get/magit/magit-pkg.el"
-;;;;;;  "../../../../../../../.emacs.d/el-get/magit/magit-stgit.el"
-;;;;;;  "../../../../../../../.emacs.d/el-get/magit/magit-svn.el"
-;;;;;;  "../../../../../../../.emacs.d/el-get/magit/magit-topgit.el"
-;;;;;;  "../../../../../../../.emacs.d/el-get/magit/magit-wip.el"
-;;;;;;  "../../../../../../../.emacs.d/el-get/magit/magit.el" "../../../../../../../.emacs.d/el-get/magit/rebase-mode.el"
-;;;;;;  "el-get/el-get-autoloads.el" "el-get/el-get-build.el" "el-get/el-get-byte-compile.el"
-;;;;;;  "el-get/el-get-core.el" "el-get/el-get-custom.el" "el-get/el-get-dependencies.el"
-;;;;;;  "el-get/el-get-install.el" "el-get/el-get-methods.el" "el-get/el-get-notify.el"
-;;;;;;  "el-get/el-get-recipes.el" "el-get/el-get-status.el" "magit/50magit.el"
-;;;;;;  "magit/magit-bisect.el" "magit/magit-key-mode.el" "magit/magit-pkg.el")
-;;;;;;  (20797 40716 591390))
+;;;### (autoloads nil nil ("../../../../../../../.emacs.d/el-get/ddskk/ccc.el"
+;;;;;;  "../../../../../../../.emacs.d/el-get/ddskk/cdb.el" "../../../../../../../.emacs.d/el-get/ddskk/context-skk.el"
+;;;;;;  "../../../../../../../.emacs.d/el-get/ddskk/install-info.el"
+;;;;;;  "../../../../../../../.emacs.d/el-get/ddskk/install.el" "../../../../../../../.emacs.d/el-get/ddskk/ptexinfmt.el"
+;;;;;;  "../../../../../../../.emacs.d/el-get/ddskk/queue-m.el" "../../../../../../../.emacs.d/el-get/ddskk/skk-abbrev.el"
+;;;;;;  "../../../../../../../.emacs.d/el-get/ddskk/skk-act.el" "../../../../../../../.emacs.d/el-get/ddskk/skk-annotation.el"
+;;;;;;  "../../../../../../../.emacs.d/el-get/ddskk/skk-auto.el"
+;;;;;;  "../../../../../../../.emacs.d/el-get/ddskk/skk-autoloads.el"
+;;;;;;  "../../../../../../../.emacs.d/el-get/ddskk/skk-azik.el"
+;;;;;;  "../../../../../../../.emacs.d/el-get/ddskk/skk-cdb.el" "../../../../../../../.emacs.d/el-get/ddskk/skk-comp.el"
+;;;;;;  "../../../../../../../.emacs.d/el-get/ddskk/skk-cursor.el"
+;;;;;;  "../../../../../../../.emacs.d/el-get/ddskk/skk-cus.el" "../../../../../../../.emacs.d/el-get/ddskk/skk-dcomp.el"
+;;;;;;  "../../../../../../../.emacs.d/el-get/ddskk/skk-develop.el"
+;;;;;;  "../../../../../../../.emacs.d/el-get/ddskk/skk-emacs.el"
+;;;;;;  "../../../../../../../.emacs.d/el-get/ddskk/skk-gadget.el"
+;;;;;;  "../../../../../../../.emacs.d/el-get/ddskk/skk-hint.el"
+;;;;;;  "../../../../../../../.emacs.d/el-get/ddskk/skk-inline.el"
+;;;;;;  "../../../../../../../.emacs.d/el-get/ddskk/skk-isearch.el"
+;;;;;;  "../../../../../../../.emacs.d/el-get/ddskk/skk-jisx0201.el"
+;;;;;;  "../../../../../../../.emacs.d/el-get/ddskk/skk-jisx0213.el"
+;;;;;;  "../../../../../../../.emacs.d/el-get/ddskk/skk-jisyo-edit-mode.el"
+;;;;;;  "../../../../../../../.emacs.d/el-get/ddskk/skk-kakasi.el"
+;;;;;;  "../../../../../../../.emacs.d/el-get/ddskk/skk-kcode.el"
+;;;;;;  "../../../../../../../.emacs.d/el-get/ddskk/skk-leim.el"
+;;;;;;  "../../../../../../../.emacs.d/el-get/ddskk/skk-look.el"
+;;;;;;  "../../../../../../../.emacs.d/el-get/ddskk/skk-lookup.el"
+;;;;;;  "../../../../../../../.emacs.d/el-get/ddskk/skk-macs.el"
+;;;;;;  "../../../../../../../.emacs.d/el-get/ddskk/skk-num.el" "../../../../../../../.emacs.d/el-get/ddskk/skk-server-completion.el"
+;;;;;;  "../../../../../../../.emacs.d/el-get/ddskk/skk-server.el"
+;;;;;;  "../../../../../../../.emacs.d/el-get/ddskk/skk-setup.el"
+;;;;;;  "../../../../../../../.emacs.d/el-get/ddskk/skk-show-mode.el"
+;;;;;;  "../../../../../../../.emacs.d/el-get/ddskk/skk-sticky.el"
+;;;;;;  "../../../../../../../.emacs.d/el-get/ddskk/skk-study.el"
+;;;;;;  "../../../../../../../.emacs.d/el-get/ddskk/skk-tankan.el"
+;;;;;;  "../../../../../../../.emacs.d/el-get/ddskk/skk-tut.el" "../../../../../../../.emacs.d/el-get/ddskk/skk-vars.el"
+;;;;;;  "../../../../../../../.emacs.d/el-get/ddskk/skk-version.el"
+;;;;;;  "../../../../../../../.emacs.d/el-get/ddskk/skk-viper.el"
+;;;;;;  "../../../../../../../.emacs.d/el-get/ddskk/skk-xemacs.el"
+;;;;;;  "../../../../../../../.emacs.d/el-get/ddskk/skk.el" "../../../../../../../.emacs.d/el-get/ddskk/vip.el")
+;;;;;;  (20797 45153 101355))
 
 ;;;***
 
--- home/.emacs.d/el-get/.status.el
+++ home/.emacs.d/el-get/.status.el
@@ -1,2 +1,10 @@
-((el-get status "installed" recipe
+((ddskk status "installed" recipe
+	(:name ddskk :website "http://openlab.ring.gr.jp/skk/ddskk.html" :description "A Japanese input method on Emacs." :type http-tar :options
+	       ("xzf")
+	       :url "http://openlab.ring.gr.jp/skk/maintrunk/ddskk-20130310.tar.gz" :autoloads nil :features
+	       ("skk-setup")
+	       :build
+	       `(,(concat el-get-emacs " -batch -q -no-site-file -l SKK-MK -f SKK-MK-compile")
+		 "mv skk-setup.el.in skk-setup.el")))
+ (el-get status "installed" recipe
 	 (:name el-get :website "https://github.com/dimitri/el-get#readme" :description "Manage the external elisp bits and pieces you depend upon." :type github :branch "4.stable" :pkgname "dimitri/el-get" :features el-get :info "." :load "el-get.el")))
