0f015c6dc9b41a032339c5a10f1b94b857afd3bf
HORINOUCHI Masato <thermes@confei.to>
2013-03-11 16:58:31 +0900

    setup el-get.

--- home/.emacs.d/init.el
+++ home/.emacs.d/init.el
@@ -1,6 +1,17 @@
 ; -*- mode: emacs-lisp; coding: utf-8 -*-
 ;;; ~/.emacs.d/init.el -- Emacsen initialize file
 
+(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
+
+(unless (require 'el-get nil 'noerror)
+  (with-current-buffer
+      (url-retrieve-synchronously
+       "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
+    (goto-char (point-max))
+    (eval-print-last-sexp)))
+
+(el-get 'sync)
+
 (add-to-list 'load-path "~/.emacs.d/elisp")
 
 (require 'info)
@@ -18,18 +29,3 @@
 ;; 40 メジャーモード
 ;; 50 マイナーモード
 ;; 90 起動後実行系
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ansi-color-names-vector ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
- '(custom-safe-themes (quote ("3ec23c5d3a7860cde51f491da4f4a749db19f91e286c0ab5539c1add0218eb74" "42c7f1aa7a3db4ab8efabf1d10c158c2f1414ac57cb207cde815eadad72170d2" default)))
- '(global-hl-line-mode t)
- '(show-paren-mode t))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
