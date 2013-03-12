2c23ac8e38bb6fa6a6dd5c492463972f464d7373
HORINOUCHI Masato <thermes@confei.to>
2013-03-12 11:15:34 +0900

    add tromey to package-archives.

--- home/.emacs.d/init.el
+++ home/.emacs.d/init.el
@@ -1,37 +1,31 @@
 ; -*- mode: emacs-lisp; coding: utf-8 -*-
 ;;; ~/.emacs.d/init.el -- Emacsen initialize file
 
+;; package.el & MELPA
 (require 'package)
-
-; Add package-archives
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
 (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
-(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/")) ; ついでにmarmaladeも追加
-
-; Initialize
+(add-to-list 'package-archives '("tromey" . "http://tromey.com/elpa/"))
 (package-initialize)
-
-; melpa.el
 (require 'melpa)
 
-
+;; el-get
 (add-to-list 'load-path "~/.emacs.d/el-get/el-get")
-
-(unless (require 'el-get nil 'noerror)
-  (with-current-buffer
-      (url-retrieve-synchronously
-       "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
-    (goto-char (point-max))
-    (eval-print-last-sexp)))
+(unless (require 'el-get nil t)
+  (url-retrieve
+   "https://raw.github.com/dimitri/el-get/master/el-get-install.el"
+   (lambda (s)
+     (goto-char (point-max))
+     (eval-print-last-sexp))))
 (el-get 'sync)
 
-
-
-
+;; local
 (add-to-list 'load-path "~/.emacs.d/elisp")
 
 (require 'info)
 (add-to-list 'Info-additional-directory-list (expand-file-name "~/.emacs.d/info"))
 
+;; run init-loader
 (require 'init-loader)
 (init-loader-load)
 
