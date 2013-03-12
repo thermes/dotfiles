db00e8c97465ccdcfc356e35900aa9fd9aa34c35
HORINOUCHI Masato <thermes@confei.to>
2013-03-12 11:36:11 +0900

    setup ruby-mode

---
+++ home/.emacs.d/inits/40_ruby.el
@@ -0,0 +1,12 @@
+; -*- mode: emacs-lisp; coding: utf-8 -*-
+
+;; ruby-mode.el
+(autoload 'ruby-mode "ruby-mode" "Major mode for ruby files" t)
+(add-to-list 'auto-mode-alist '("\\.rb$" . ruby-mode))
+(add-to-list 'interpreter-mode-alist '("ruby" . ruby-mode))
+
+;; inf-ruby.el
+(autoload 'inf-ruby "inf-ruby" "Run an inferior Ruby process" t)
+(autoload 'inf-ruby-setup-keybindings "inf-ruby" "" t)
+(eval-after-load 'ruby-mode
+  '(add-hook 'ruby-mode-hook 'inf-ruby-setup-keybindings))
