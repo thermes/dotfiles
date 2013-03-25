c4e5bca26b781021624bba251d39567d56cd4bd8
HORINOUCHI Masato <thermes@confei.to>
2013-03-25 21:41:45 +0900

    change enable linum-mode.

--- home/.emacs.d/inits/10_appearance.el
+++ home/.emacs.d/inits/10_appearance.el
@@ -6,6 +6,10 @@
 ;; Toggle line highlighting in all buffers (Global Hl-Line mode).
 (global-hl-line-mode)
 
+;; Toggle Linum mode in all buffers.
+(global-linum-mode t)
+(setq linum-format "%4d")
+
 ;; Non-nil if Show-Paren mode is enabled.
 (show-paren-mode t)
 (setq show-paren-style 'mixed)
