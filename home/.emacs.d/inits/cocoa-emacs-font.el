1c556fcf725f91c415367ce9127032692a113298
HORINOUCHI Masato <thermes@confei.to>
2013-04-06 22:57:14 +0900

    change CocoaEmacs のフォント設定を変更。

--- home/.emacs.d/inits/cocoa-emacs-font.el
+++ home/.emacs.d/inits/cocoa-emacs-font.el
@@ -1,4 +1,14 @@
 ; -*- mode: emacs-lisp; coding: utf-8 -*-
 
-(set-face-attribute 'default nil :family "Ricty" :height 140)
-(set-fontset-font nil 'japanese-jisx0208 (font-spec :family "Ricty"))
+(create-fontset-from-ascii-font
+ "Ricty-14:weight=normal:slant=normal" nil "ricty")
+(dolist (target '(japanese-jisx0208
+		  japanese-jisx0212
+		  japanese-jisx0213.2004-1
+		  japanese-jisx0213-2
+		  katakana-jisx0201))
+  (set-fontset-font "fontset-ricty"
+		    target
+		    (font-spec :family "Ricty")
+		    nil 'append))
+(add-to-list 'default-frame-alist '(font . "fontset-ricty"))
