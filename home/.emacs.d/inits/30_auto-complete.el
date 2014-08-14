6c8fc0b0b9508cd19254365b2a2be91c7387bcb4
HORINOUCHI Masato <thermes@confei.to>
2014-08-14 19:28:42 +0900

    ac-dict の場所を変更。

--- home/.emacs.d/inits/30_auto-complete.el
+++ home/.emacs.d/inits/30_auto-complete.el
@@ -1,6 +1,6 @@
 ; -*- mode: emacs-lisp; coding: utf-8 -*-
 
 (when (require 'auto-complete-config nil t)
-  (add-to-list 'ac-dictionary-directories "~/.emacs.d/elisp/ac-dict")
+  (add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
   (define-key ac-mode-map (kbd "M-TAB") 'auto-complete)
   (ac-config-default))
