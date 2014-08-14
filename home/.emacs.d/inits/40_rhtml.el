57ec803eed9b6114339e396fe1a49e814997f17e
HORINOUCHI Masato <thermes@confei.to>
2014-08-14 19:27:11 +0900

    ~/.emacs.d/elisp/rhtml を削除。

--- home/.emacs.d/inits/40_rhtml.el
+++ home/.emacs.d/inits/40_rhtml.el
@@ -1,6 +1,5 @@
 ; -*- mode: emacs-lisp; coding: utf-8 -*-
 
-(add-to-list 'load-path "~/.emacs.d/elisp/rhtml")
 (when (require 'rhtml-mode nil t)
   (add-hook 'rhtml-mode-hook
 	    (lambda () (rinari-launch))))
