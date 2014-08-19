a86c8e8536af8063c306325a8f8d82f7b2142e93
HORINOUCHI Masato <thermes@confei.to>
2014-08-19 18:25:10 +0900

    emacs -nw 環境だと font設定に失敗するのを修正。

--- home/.emacs.d/inits/linux-font.el
+++ home/.emacs.d/inits/linux-font.el
@@ -1,18 +1,19 @@
 ;;; linux-font.el
 
-(create-fontset-from-ascii-font "Ricty-12:weight=normal:slant=normal"
-                                nil
-                                "ricty")
-(dolist (target '(japanese-jisx0208
-		  japanese-jisx0212
-		  japanese-jisx0213.2004-1
-		  japanese-jisx0213-2
-		  katakana-jisx0201))
-  (set-fontset-font "fontset-ricty"
-		    target
-		    (font-spec :family "Ricty")
-		    nil 'append))
-(add-to-list 'default-frame-alist '(font . "fontset-ricty"))
+(when (window-system)
+  (create-fontset-from-ascii-font "Ricty-12:weight=normal:slant=normal"
+                                  nil
+                                  "ricty")
+  (dolist (target '(japanese-jisx0208
+                    japanese-jisx0212
+                    japanese-jisx0213.2004-1
+                    japanese-jisx0213-2
+                    katakana-jisx0201))
+    (set-fontset-font "fontset-ricty"
+                      target
+                      (font-spec :family "Ricty")
+                      nil 'append))
+  (add-to-list 'default-frame-alist '(font . "fontset-ricty")))
 
 ;; Local Variables:
 ;; coding: utf-8
