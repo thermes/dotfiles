843f77fdafb1e9cbe38751ffa7aecf876099d372
HORINOUCHI Masato <thermes@confei.to>
2014-08-19 17:58:44 +0900

    Install wgrep

---
+++ home/.emacs.d/inits/30_wgrep.el
@@ -0,0 +1,11 @@
+;;; 30_wgrep.el
+
+(when (require 'wgrep nil t)
+  (setq wgrep-enable-key "r"))
+
+;; Local Variables:
+;; coding: utf-8
+;; indent-tabs-mode: nil
+;; End:
+
+;;; 30_wgrep.el ends here
