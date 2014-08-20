5183170dba97e7fc1013fc1920182addb775222e
HORINOUCHI Masato <thermes@confei.to>
2014-08-20 18:17:04 +0900

    require に失敗しても大丈夫なので when しないように修正。

--- home/.emacs.d/inits/30_helm.el
+++ home/.emacs.d/inits/30_helm.el
@@ -1,15 +1,15 @@
 ;;; 30_helm.el
 
-(when (require 'helm-config nil t)
-  (helm-mode 1)
+;; helm
+(require 'helm-config)
+(helm-mode 1)
 
-  (define-key global-map (kbd "M-x") 'helm-M-x)
-  (define-key global-map (kbd "M-y") 'helm-show-kill-ring)
-  (define-key global-map (kbd "C-x b") 'helm-buffers-list)
+(define-key global-map (kbd "M-x") 'helm-M-x)
+(define-key global-map (kbd "M-y") 'helm-show-kill-ring)
+(define-key global-map (kbd "C-x b") 'helm-buffers-list)
 
-  (define-key helm-map (kbd "C-h") 'delete-backward-char)
-  (define-key helm-map "[TAB]" 'helm-execute-persistent-action)
-  )
+(define-key helm-map (kbd "C-h") 'delete-backward-char)
+(define-key helm-map "[TAB]" 'helm-execute-persistent-action)
 
 ;; helm-ag
 (require 'helm-ag)
