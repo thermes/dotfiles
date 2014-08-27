eb9aa8afc8f30042c6ce32550c9d1d726fb8e2f1
HORINOUCHI Masato <thermes@confei.to>
2014-08-27 16:34:34 +0900

    setup helm

--- home/.emacs.d/inits/30_helm.el
+++ home/.emacs.d/inits/30_helm.el
@@ -9,12 +9,10 @@
 (define-key global-map (kbd "C-c h") 'helm-mini)
 (define-key global-map (kbd "M-x") 'helm-M-x)
 (define-key global-map (kbd "M-y") 'helm-show-kill-ring)
-(define-key global-map (kbd "C-x b") 'helm-buffers-list)
+(define-key global-map (kbd "C-x C-b") 'helm-buffers-list)
 
 (define-key helm-map (kbd "C-h") 'delete-backward-char)
 (define-key helm-find-files-map (kbd "C-h") 'delete-backward-char)
-
-(setq helm-buffer-max-length 50)
 
 (set-face-attribute 'helm-selection nil
                     :background "#fdf6e3")
