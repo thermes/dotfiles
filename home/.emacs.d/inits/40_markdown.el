7c632db0861e58594de7a00ce0e7359f3f8083aa
HORINOUCHI Masato <thermes@confei.to>
2014-09-09 14:50:42 +0900

    setup markdown-mode

---
+++ home/.emacs.d/inits/40_markdown.el
@@ -0,0 +1,14 @@
+;;; 40_markdown.el
+
+(autoload 'markdown-mode "markdown-mode"
+  "Major mode for editing Markdown files" t)
+(add-to-list 'auto-mode-alist '("\\.text\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+
+;; Local Variables:
+;; coding: utf-8
+;; indent-tabs-mode: nil
+;; End:
+
+;;; 40_markdown.el ends here
