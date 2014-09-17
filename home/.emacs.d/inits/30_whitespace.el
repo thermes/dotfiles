362360b52d1b9c145ee80a5b40c0d86f9fc7c94c
HORINOUCHI Masato <thermes@confei.to>
2014-09-17 13:32:33 +0900

    tab文字を暗い色に修正。

--- home/.emacs.d/inits/30_whitespace.el
+++ home/.emacs.d/inits/30_whitespace.el
@@ -4,7 +4,7 @@
 (custom-set-variables '(whitespace-style '(face tabs trailing space-before-tab tab-mark)))
 
 (set-face-attribute 'whitespace-tab nil
-                    :foreground "#259185"
+                    :foreground "#205050"
                     :background nil)
 (set-face-attribute 'whitespace-trailing nil
                     :foreground "#a57705"
