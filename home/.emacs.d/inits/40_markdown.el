586b698de343d4c46239a5d863b4decd07771c60
HORINOUCHI Masato <thermes@confei.to>
2014-09-17 13:30:23 +0900

    tab ではなく 4space に変更。

--- home/.emacs.d/inits/40_markdown.el
+++ home/.emacs.d/inits/40_markdown.el
@@ -7,6 +7,11 @@
 (add-to-list 'auto-mode-alist '("\\.markdown\\'" . gfm-mode))
 (add-to-list 'auto-mode-alist '("\\.md\\'" . gfm-mode))
 
+(add-hook 'gfm-mode-hook
+          '(lambda ()
+             (setq indent-tabs-mode nil)
+             (setq tab-width 4)))
+
 ;; Local Variables:
 ;; coding: utf-8
 ;; indent-tabs-mode: nil
