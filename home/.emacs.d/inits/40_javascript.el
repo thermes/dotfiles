199ddbf929ef9b3de1cd6f8bb1792c05d56e7cee
HORINOUCHI Masato <thermes@confei.to>
2014-09-17 13:33:04 +0900

    8tab → 4tab に変更。

--- home/.emacs.d/inits/40_javascript.el
+++ home/.emacs.d/inits/40_javascript.el
@@ -4,6 +4,7 @@
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
 (add-hook 'js-mode-hook 'js2-minor-mode)
 (add-to-list 'interpreter-mode-alist '("node" . js2-mode))
+(add-hook 'js2-mode-hook '(lambda () (custom-set-variables '(tab-width 4))))
 
 (require 'coffee-mode)
 (custom-set-variables '(coffee-tab-width 2))
