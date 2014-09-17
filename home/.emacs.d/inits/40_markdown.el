f4a9be88d3be4960d89395018ce7c7017f76fca6
HORINOUCHI Masato <thermes@confei.to>
2014-09-17 13:29:47 +0900

    markdown-mode → gfm-mode に変更。

--- home/.emacs.d/inits/40_markdown.el
+++ home/.emacs.d/inits/40_markdown.el
@@ -2,9 +2,10 @@
 
 (autoload 'markdown-mode "markdown-mode"
   "Major mode for editing Markdown files" t)
-(add-to-list 'auto-mode-alist '("\\.text\\'" . markdown-mode))
-(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
-(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.txt\\'" . gfm-mode))
+(add-to-list 'auto-mode-alist '("\\.text\\'" . gfm-mode))
+(add-to-list 'auto-mode-alist '("\\.markdown\\'" . gfm-mode))
+(add-to-list 'auto-mode-alist '("\\.md\\'" . gfm-mode))
 
 ;; Local Variables:
 ;; coding: utf-8
