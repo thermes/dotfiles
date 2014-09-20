27099d89e5fecbe99f7009bddb9972df18ada797
HORINOUCHI Masato <thermes@confei.to>
2014-09-20 22:01:03 +0900

    cask を追加。

--- brew_cask_install.sh
+++ brew_cask_install.sh
@@ -16,17 +16,26 @@ brew tap caskroom/versions
 ## installer
 brew cask install adobe-reader
 brew cask install karabiner
+brew cask install sdformatter
 brew cask install vagrant
 brew cask install virtualbox
 
 ## symbolic link
+brew cask install alfred
+brew cask install appcleaner
+brew cask install caffeine
+brew cask install ccleaner
+brew cask install coteditor
 brew cask install cyberduck
-brew cask install dash
 brew cask install dropbox
-brew cask install evernote
 brew cask install firefox-ja
 brew cask install google-chrome
+brew cask install google-earth
 brew cask install iterm2
 brew cask install libreoffice
+brew cask install magican
 brew cask install onyx
+brew cask install the-unarchiver
+brew cask install thunderbird-ja
 brew cask install vlc
+brew cask install xld
