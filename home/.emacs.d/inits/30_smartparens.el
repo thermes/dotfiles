d80587593645fc01750a27b14c14817ed0e1c1b4
HORINOUCHI Masato <thermes@confei.to>
2014-10-10 18:58:53 +0900

    smartparens の設定変更。

--- home/.emacs.d/inits/30_smartparens.el
+++ home/.emacs.d/inits/30_smartparens.el
@@ -1,6 +1,10 @@
 ;;; 30_smartparens.el
 
+(require 'smartparens-config)
 (smartparens-global-mode t)
+
+;; highlights matching pairs
+(show-smartparens-global-mode t)
 
 ;; Local Variables:
 ;; coding: utf-8
