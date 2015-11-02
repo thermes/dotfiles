0322dbc2bdf8c09ac3e5db7178cff6db4a03836f
HORINOUCHI Masato <thermes@confei.to>
2015-11-02 19:10:07 +0900

    Emacs font change from Ricty to Myrica.

--- home/.emacs.d/inits/cocoa-emacs-font.el
+++ home/.emacs.d/inits/cocoa-emacs-font.el
@@ -1,19 +1,19 @@
 ;;; cocoa-emacs-font.el
 
 (when window-system
-  (create-fontset-from-ascii-font "Ricty-14:weight=normal:slant=normal"
+  (create-fontset-from-ascii-font "Myrica M-14:weight=normal:slant=normal"
                                   nil
-                                  "ricty")
+                                  "myrica")
   (dolist (target '(japanese-jisx0208
                     japanese-jisx0212
                     japanese-jisx0213.2004-1
                     japanese-jisx0213-2
                     katakana-jisx0201))
-    (set-fontset-font "fontset-ricty"
+    (set-fontset-font "fontset-myrica"
                       target
-                      (font-spec :family "Ricty")
+                      (font-spec :family "Myrica M")
                       nil 'append))
-  (add-to-list 'default-frame-alist '(font . "fontset-ricty")))
+  (add-to-list 'default-frame-alist '(font . "fontset-myrica")))
 
 ;; Local Variables:
 ;; coding: utf-8
