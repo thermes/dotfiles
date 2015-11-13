d4fc9f1c48d082c0d821036f08dc666fc289b091
HORINOUCHI Masato <thermes@confei.to>
2015-11-13 18:50:42 +0900

    Debian/Ubuntu package install script

---
+++ deb_install.sh
@@ -0,0 +1,38 @@
+#! /bin/sh
+
+### Debian/Ubuntu packages
+
+## update
+apt-get update
+
+## aptitude
+apt-get -y install aptitude
+
+## Zsh
+apt-get -y install zsh
+
+## tmux
+apt-get -y install tmux
+
+## Emacs
+apt-get -y install emacs
+
+## Git
+apt-get -y install git
+apt-get -y install tig
+
+## misc
+apt-get -y install colordiff
+apt-get -y install discount
+apt-get -y install lftp
+apt-get -y install lv
+apt-get -y install mosh
+apt-get -y install nkf
+apt-get -y install silversearcher-ag
+apt-get -y install w3m
+apt-get -y install whois
+
+## debian
+apt-get -y install manpages-ja
+apt-get -y install manpages-ja-dev
+apt-get -y install util-linux-locales
