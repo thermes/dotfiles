7c09ee3cc8bb328781997f46cbb1aaba0786cd05
HORINOUCHI Masato <thermes@confei.to>
2016-11-17 18:38:18 +0900

    setup zenburn theme.

---
+++ home/.spacemacs.d/inits/40_zenburn.el
@@ -0,0 +1,14 @@
+;;; 40_zenburn.el
+
+(zenburn-with-color-variables
+  (custom-theme-set-faces
+   'zenburn
+   `(region ((,class (:background ,zenburn-bg+3))
+             (t :inverse-video nil)))))
+
+;; Local Variables:
+;; coding: utf-8
+;; indent-tabs-mode: nil
+;; End:
+
+;;; 40_zenburn.el ends here
