2d24385d21eabb0ea8c853cdc7422424fb9f05fe
HORINOUCHI Masato <thermes@confei.to>
2017-08-13 19:09:32 +0900

    add yaml mode.

--- home/.spacemacs.d/init.el
+++ home/.spacemacs.d/init.el
@@ -60,6 +60,7 @@ values."
      syntax-checking
      (version-control :variables
                       version-control-diff-tool 'diff-hl)
+     yaml
      )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
