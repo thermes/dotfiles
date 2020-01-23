00131c549e70cf539e48b4e563fbeb7aecdaeb45
HORINOUCHI Masato <thermes@confei.to>
2020-01-23 23:42:37 +0900

    change font size 16 -> 13.5 (by Linux Environment)

--- home/.spacemacs.d/init.el
+++ home/.spacemacs.d/init.el
@@ -156,7 +156,7 @@ values."
    ;; Default font, or prioritized list of fonts. `powerline-scale' allows to
    ;; quickly tweak the mode-line size to make separators look not too crappy.
    dotspacemacs-default-font '("Cica"
-                               :size 16.0
+                               :size 13.5
                                :weight normal
                                :width normal
                                :powerline-scale 1.0)
