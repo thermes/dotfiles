cb2eb79d939b578fe342c5355e53c584d68304b9
HORINOUCHI Masato <thermes@confei.to>
2020-05-20 00:10:58 +0900

    change apt-get → apt

--- .install/deb_install.sh
+++ .install/deb_install.sh
@@ -3,55 +3,55 @@
 ### Debian/Ubuntu packages
 
 ## aptitude
-apt-get -y install aptitude
+apt -y install aptitude
 
 ## rcm
-apt-get -y install rcm
+apt -y install rcm
 
 ## Zsh
-apt-get -y install zsh
-apt-get -y install fzf
-apt-get -y install fasd
+apt -y install zsh
+apt -y install fzf
+apt -y install fasd
 
 ## tmux
-apt-get -y install tmux
+apt -y install tmux
 
 ## Emacs
-apt-get -y install emacs-nox emacs-common-non-dfsg
+apt -y install emacs-nox emacs-common-non-dfsg
 
 ## Git
-apt-get -y install git
-apt-get -y install tig
+apt -y install git
+apt -y install tig
 
 ## rbenv
-apt-get -y install build-essential
-apt-get -y install libreadline6-dev
-apt-get -y install zlib1g-dev
-apt-get -y install libsqlite3-dev
-apt-get -y install libssl-dev
-apt-get -y install libxml2-dev
-apt-get -y install libxslt-dev
+apt -y install build-essential
+apt -y install libreadline6-dev
+apt -y install zlib1g-dev
+apt -y install libsqlite3-dev
+apt -y install libssl-dev
+apt -y install libxml2-dev
+apt -y install libxslt-dev
 
 ## ranger
-apt-get -y install ranger highlight mediainfo poppler-utils w3m
+apt -y install ranger highlight mediainfo poppler-utils w3m
 
 ## misc
-apt-get -y install autossh
-apt-get -y install colordiff
-apt-get -y install curl
-apt-get -y install discount
-apt-get -y install jq
-apt-get -y install lftp
-apt-get -y install lv
-apt-get -y install mosh
-apt-get -y install nkf
-apt-get -y install ripgrep
-apt-get -y install silversearcher-ag
-apt-get -y install unar
-apt-get -y install whois
-apt-get -y install zip unzip
+apt -y install autossh
+apt -y install colordiff
+apt -y install curl
+apt -y install discount
+apt -y install jq
+apt -y install lftp
+apt -y install lv
+apt -y install mosh
+apt -y install nkf
+apt -y install ripgrep
+apt -y install silversearcher-ag
+apt -y install unar
+apt -y install whois
+apt -y install zip unzip
 
 ## debian
-apt-get -y install manpages-ja
-apt-get -y install manpages-ja-dev
-apt-get -y install util-linux-locales
+apt -y install manpages-ja
+apt -y install manpages-ja-dev
+apt -y install util-linux-locales
