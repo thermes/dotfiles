8bc793b10a84f0644b8558db22d3187889b0cd56
HORINOUCHI Masato <thermes@confei.to>
2022-06-19 21:30:22 +0900

    setup doom emacs

--- doom.d/config.el
+++ doom.d/config.el
@@ -32,7 +32,7 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-one)
+(setq doom-theme 'doom-zenburn)
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
@@ -74,5 +74,8 @@
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
+(set-language-environment "Japanese")
+(prefer-coding-system 'utf-8)
 
 (define-key key-translation-map (kbd "C-h") (kbd "<DEL>"))
+
