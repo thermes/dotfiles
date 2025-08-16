fe7abe220378dd1f88771d71f651de6dbc43c311
HORINOUCHI Masato <thermes@confei.to>
2025-08-16 22:15:25 +0900

    Change 1Password ssh-agent.

--- zsh/90_aliases.zsh
+++ zsh/90_aliases.zsh
@@ -29,6 +29,11 @@ alias sudo='nocorrect sudo'
 alias vagrant='nocorrect vagrant'
 alias w3m='nocorrect w3m'
 
+if grep -q "microsoft" /proc/version; then
+    alias ssh='ssh.exe'
+    alias ssh-add='ssh-add.exe'
+fi
+
 if [[ -x `whence -p colordiff` ]]; then
     alias diff='colordiff'
 fi
