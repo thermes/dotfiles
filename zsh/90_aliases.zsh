7b385a77e3599a5444b81c40546d71e01be1c825
HORINOUCHI Masato <thermes@confei.to>
2025-10-30 02:29:33 +0900

    Delete aliases that use ssh on Windows in wsl2 environment

--- zsh/90_aliases.zsh
+++ zsh/90_aliases.zsh
@@ -28,12 +28,6 @@ alias sudo='nocorrect sudo'
 alias vagrant='nocorrect vagrant'
 alias w3m='nocorrect w3m'
 
-if grep -q "microsoft" /proc/version; then
-    alias ssh='ssh.exe'
-    alias scp='scp.exe'
-    alias ssh-add='ssh-add.exe'
-fi
-
 if [[ -x `whence -p colordiff` ]]; then
     alias diff='colordiff'
 fi
