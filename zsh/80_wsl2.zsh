c36062183ea1379a49ab6fd5c36779f3ef83e152
HORINOUCHI Masato <thermes@confei.to>
2025-11-10 05:00:09 +0900

    Change enable ssh-agent forward

--- zsh/80_wsl2.zsh
+++ zsh/80_wsl2.zsh
@@ -1,5 +1,7 @@
 #! /bin/zsh
 
 if grep -q "microsoft" /proc/version; then
-    eval $($HOME/.local/bin/wsl2-ssh-agent)
+    if [[ -z "$SSH_AUTH_SOCK" ]]; then
+        eval $($HOME/.local/bin/wsl2-ssh-agent)
+    fi
 fi
