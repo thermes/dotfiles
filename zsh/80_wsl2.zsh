d129a98fbee86b15229606490c7c8ba9812a6575
HORINOUCHI Masato <thermes@confei.to>
2025-11-11 22:03:50 +0900

    Add set up VS Code for use in the WSL environment

--- zsh/80_wsl2.zsh
+++ zsh/80_wsl2.zsh
@@ -4,4 +4,9 @@ if [[ -n "$WSL_DISTRO_NAME" ]]; then
     if [[ -z "$SSH_AUTH_SOCK" ]]; then
         eval $($HOME/.local/bin/wsl2-ssh-agent)
     fi
+
+    if [[ -x "`whence -p code`" ]]; then
+        export EDITOR="code --wait"
+        export VISUAL=$EDITOR
+    fi
 fi
